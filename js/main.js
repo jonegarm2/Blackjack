baf05e3ee7dafcd78aede96fe3130044c722e3ec
Jonathan Garman <jonjon@Taylors-MacBook-Pro-2.local>
2018-03-26 12:31:29 -0700

    cards update/css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 class Card {
-    constructor(suit, face, value) {
-      this.suit = suit;
+    constructor(face, value) {
       this.face = face;
       this.value = value;
     }
   }
   
   var deck = [];
+  deck = deck.map(card => card.face = card.suit.toLowerCase() + card.face)
   
-  var suits = ['C', 'S', 'D', 'H'];
-  var faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  var suits = ['c', 's', 'd', 'h'];
+  var faces = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
   
   suits.forEach(function(suit) {
     faces.forEach(function(face){
@@ -17,7 +17,7 @@ class Card {
       if (isNaN(val)) {
         val = face === 'A' ? 11 : 10;
       }
-      deck.push(new Card(suit, face, val));
+      deck.push(new Card(suit + face, val));
     });
   });
   
@@ -25,23 +25,71 @@ class Card {
 
 
   /*----- constants -----*/
+  
 
 
 
 /*----- app's state (variables) -----*/
+var deck, bankroll, bet, dealerHand, playerHand;
+
+
+//Need to adjust for the player card
+
+
 
 
 
 /*----- cached element references -----*/
+
+var dealerCardsEl= document.getElementById('dealer-cards')
+var playerCardsEl= document.getElementById('player-cards')
 
 
 
 
 
 /*----- event listeners -----*/
-
+//click for deal
+//click for betting
+//click for hit/stand/double
+document.getElementById('option2').addEventListener('click', betting);
+document.getElementById('option1').addEventListener('click', gameplay);
 
 
 
 
 /*----- functions -----*/
+// Connect betting to click
+function betting(evt) {
+  let placeBet = parseInt(evt.target.textContent);
+  console.log(placeBet);
+
+  if (placeBet > bankroll) {
+      return;
+  } else if (bankroll > 0) {
+      bankroll = (bankroll -= placeBet);
+      bet = (bet += placeBet);
+  }
+
+  if (bankroll === 0) msg = 'You lost it all!'
+
+  function gameplay(evt) {
+    //let button deal with the gameplay buttons
+    //if (deal is clicked)
+    //    return deal/shuffle function
+    //else disable button until end of hand
+    //if hit add another card to the player-cards
+  }
+
+  render(); {
+    deck.forEach(function(card, idx) {
+      html += `<div class="card ${idx ? card.face : 'back'}"></div>`;
+    });
+  }
+
+}
+
+//shuffle function
+function initialize() {
+    bankroll = '$200.00'
+}
